d75f999793eab98f8025553e376aece2149e5761
chase <chase>
2020-05-23 16:47:52 +0300

    assembly info

--- SnowPakTool/Properties/AssemblyInfo.cs
+++ SnowPakTool/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle ( "SnowPakTool" )]
-[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyDescription ( "SnowRunner .pak and misc. tools" )]
 [assembly: AssemblyConfiguration ( "" )]
 [assembly: AssemblyCompany ( "" )]
 [assembly: AssemblyProduct ( "SnowPakTool" )]
-[assembly: AssemblyCopyright ( "Copyright © 2020" )]
+[assembly: AssemblyCopyright ( "Copyright © 2020 chase <s.chubukov@protonmail.com>" )]
 [assembly: AssemblyTrademark ( "" )]
 [assembly: AssemblyCulture ( "" )]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible ( false )]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ( "491aaeb0-613a-4a27-bfcd-69c2480d13c7" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ( "1.0.0.0" )]
 [assembly: AssemblyFileVersion ( "1.0.0.0" )]
