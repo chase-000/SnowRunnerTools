b8e6373c61f55f269e5aa1bd0d57f09b0e1cf0f1
chase <chase>
2020-05-24 18:18:17 +0300

    some assembly-level attributes moved into SolutionInfo.cs

--- SnowPakTool/Properties/AssemblyInfo.cs
+++ SnowPakTool/Properties/AssemblyInfo.cs
@@ -3,9 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle ( "SnowPakTool" )]
 [assembly: AssemblyDescription ( "SnowRunner .pak and misc. tools" )]
-[assembly: AssemblyConfiguration ( "" )]
 [assembly: AssemblyProduct ( "SnowPakTool" )]
-[assembly: AssemblyCopyright ( "Copyright © 2020 chase <s.chubukov@protonmail.com>" )]
 [assembly: ComVisible ( false )]
 [assembly: Guid ( "491aaeb0-613a-4a27-bfcd-69c2480d13c7" )]
 [assembly: AssemblyVersion ( "1.0.0.0" )]
