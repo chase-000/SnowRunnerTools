e18f67442a7111b7e260d10f5a91ed4039b9823c
chase <chase>
2020-05-31 00:00:32 +0300

    PAKs creation no longer uses ZipArchive
    no compression anymore, no ZIP64 support

---
+++ SnowPakTool/PakableFileNameComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnowPakTool {
+
+	public class PakableFileNameComparer : IComparer<string> {
+
+		public static PakableFileNameComparer Instance { get; } = new PakableFileNameComparer ();
+
+		private PakableFileNameComparer () { }
+
+		public int Compare ( string x , string y ) {
+			var first = ZipPakHelper.LoadListName.Equals ( x , StringComparison.OrdinalIgnoreCase );
+			var second = ZipPakHelper.LoadListName.Equals ( y , StringComparison.OrdinalIgnoreCase );
+			if ( first ) {
+				if ( second ) return 0;
+				return -1;
+			}
+			if ( second ) return 1;
+			return StringComparer.OrdinalIgnoreCase.Compare ( x , y );
+		}
+
+	}
+
+}
--- SnowPakTool/ZipPakHelper.cs
+++ SnowPakTool/ZipPakHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace SnowPakTool {
@@ -13,6 +14,13 @@ namespace SnowPakTool {
 		private const int ZipVersion = 0x14;
 
 
+		/// <remarks>
+		/// <see cref="System.IO.Compression.ZipArchive"/> does not support real store (0) method and uses deflate instead.
+		/// This can be worked around by first creating an archive with just the pak.load_list, and then updating it,
+		/// but the update mode runs out of memory with large archives, so it's either large archives without pak.load_list,
+		/// or an ordered with one, but only of a certain size. More than that, large archives that it creates (shared_textures.pak)
+		/// can't be read properly by the game, causing texture corruption inside garage.
+		/// </remarks>
 		public static void CreatePak ( string sourceDirectory , string pakLocation ) {
 			if ( sourceDirectory is null ) throw new ArgumentNullException ( nameof ( sourceDirectory ) );
 			if ( pakLocation is null ) throw new ArgumentNullException ( nameof ( pakLocation ) );
@@ -29,55 +37,36 @@ namespace SnowPakTool {
 			}
 
 			using ( var zipStream = File.Open ( pakLocation , FileMode.CreateNew , FileAccess.ReadWrite , FileShare.None ) ) {
-				if ( loadListExists ) {
-					WriteListFile ( zipStream , sourceDirectory , listLocation );
-				}
-				using ( var zip = new ZipArchive ( zipStream , ZipArchiveMode.Update ) ) {
-					AddFiles ( zip , sourceDirectory );
-					Console.WriteLine ( "Compressing..." );
-				}
+				var files = GetFiles ( sourceDirectory );
+				StoreFiles ( zipStream , files );
 			}
 			Console.WriteLine ( "Done." );
 		}
 
 
-		private static void AddFiles ( ZipArchive zip , string sourceDirectory ) {
-			var files = Directory.GetFiles ( sourceDirectory , "*" , SearchOption.AllDirectories );
-			var skipped = false;
-			for ( int i = 0; i < files.Length; i++ ) {
-				var location = files[i];
-				Console.Write ( $"\rAdding file {i + 1}/{files.Length}" );
-				var relativeName = location.Substring ( sourceDirectory.Length );
-				if ( !skipped && relativeName.Equals ( LoadListName , StringComparison.OrdinalIgnoreCase ) ) {
-					skipped = true;
-					continue;
-				}
-				zip.CreateEntryFromFile ( location , relativeName , CompressionLevel.Fastest );
-			}
-			Console.WriteLine ();
+		private static List<KeyValuePair<string , string>> GetFiles ( string location ) {
+			location = IOHelpers.NormalizeDirectory ( location );
+			var files = Directory.EnumerateFiles ( location , "*" , SearchOption.AllDirectories );
+			return files
+				.Select ( a => new KeyValuePair<string , string> ( a.Substring ( location.Length ) , a ) )
+				.OrderBy ( a => a.Key , PakableFileNameComparer.Instance ) //The list file must be the first file in the archive, stored without compression.
+				.ToList ()
+				;
 		}
 
-		/// <summary>
-		/// The list file must be the first file in the archive, stored without compression.
-		/// </summary>
-		/// <remarks>
-		/// <see cref="ZipArchive"/> does not support real store (0) method and uses deflate instead, so it's written here manually.
-		/// </remarks>
-		private static void WriteListFile ( Stream zipStream , string baseLocation , string listLocation ) {
-			StoreFiles ( zipStream , baseLocation , listLocation );
-		}
-
-		private static void StoreFiles ( Stream zipStream , string baseLocation , params string[] files ) {
-			var count = MiscHelpers.EnsureFitsUInt16 ( files.Length );
+		private static void StoreFiles ( Stream zipStream , IReadOnlyList<KeyValuePair<string , string>> files ) {
+			var count = MiscHelpers.EnsureFitsUInt16 ( files.Count );
 			var localEntries = new LocalFileHeader[count];
 			var relativeNames = new byte[count][];
 			var offsets = new long[count];
 
 			//local entries and actual data
+			Console.Write ( "Writing files." );
 			for ( int i = 0; i < count; i++ ) {
+				Console.Write ( $"\rWriting files: {i + 1}/{count}" );
 				offsets[i] = zipStream.Position;
-				var location = files[i];
-				var relativeName = location.Substring ( baseLocation.Length );
+				var location = files[i].Value;
+				var relativeName = files[i].Key;
 				using ( var sourceStream = File.OpenRead ( location ) ) {
 					var size = MiscHelpers.EnsureFitsUInt32 ( sourceStream.Length );
 					var relativeNameBytes = relativeNames[i] = MiscHelpers.Encoding.GetBytes ( relativeName );
@@ -101,10 +90,13 @@ namespace SnowPakTool {
 					sourceStream.CopyTo ( zipStream );
 				}
 			}
+			Console.WriteLine ();
 
 			//central directory entries
+			Console.Write ( "Writing central directory." );
 			var centralDirectoryStart = MiscHelpers.EnsureFitsUInt32 ( zipStream.Position );
 			for ( int i = 0; i < count; i++ ) {
+				Console.Write ( $"\rWriting central directory: {i + 1}/{count}" );
 				var lfh = localEntries[i];
 				var cdfh = new CentralDirectoryFileHeader ( lfh ) {
 					LocalOffset = MiscHelpers.EnsureFitsUInt32 ( offsets[i] ) ,
@@ -114,6 +106,7 @@ namespace SnowPakTool {
 				var nameBytes = relativeNames[i];
 				zipStream.Write ( nameBytes , 0 , nameBytes.Length );
 			}
+			Console.WriteLine ();
 
 			//central directory end
 			var centralDirectoryEnd = MiscHelpers.EnsureFitsUInt32 ( zipStream.Position );
