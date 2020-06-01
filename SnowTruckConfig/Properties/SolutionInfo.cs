28421f7b6a5b8008ebea93ff1b273af89e8a8605
chase <chase>
2020-06-01 22:33:10 +0300

    switched to net core

--- SnowTruckConfig/Properties/SolutionInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCopyright ( "Copyright © 2020 chase <s.chubukov@protonmail.com>" )]
-
-#if DEBUG
-[assembly: AssemblyConfiguration ( "DEBUG" )]
-#else
-[assembly: AssemblyConfiguration ( "RELEASE" )]
-#endif
